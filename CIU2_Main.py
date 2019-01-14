700c2108451ed4145a2aaf9c074b29384f3fae59
dpolasky <dpolasky@umich.edu>
2019-01-14 17:24:07 -0500

    Prevent excess loading messages on multithread

--- CIU2_Main.py
+++ CIU2_Main.py
@@ -19,7 +19,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """
-print('Loading CIUSuite 2...')
+if __name__ == '__main__':
+    print('Loading CIUSuite 2...')
 import pygubu
 import tkinter as tk
 from tkinter import filedialog
